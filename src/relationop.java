f0c27ec6d39e5778b20090ea3f67095d99b083a7
Rakshith Hegde <rakshithhegdeks@gmail.com>
2023-02-21 16:50:22 +0530

    relationoperator

---
+++ src/relationop.java
@@ -0,0 +1,16 @@
+public class relationop {
+    public static void main(String[] args) {
+        int r = 9;
+        int y = 8;
+
+        boolean result = r <= y;
+        boolean r1 = r >= y;
+        boolean r2 = r == y;
+
+        System.out.println(result);
+        System.out.println(r1);
+        System.out.println(r2);
+
+        // also works with other data types
+    }
+}
