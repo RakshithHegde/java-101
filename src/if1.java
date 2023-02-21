18c5f3cbdfc23f10ec36d06ec3aac74b39be0a16
Rakshith Hegde <rakshithhegdeks@gmail.com>
2023-02-21 17:34:13 +0530

    if

---
+++ src/if1.java
@@ -0,0 +1,13 @@
+public class if1 {
+    public static void main(String[] args) {
+        int param = 28;
+
+        if (param > 10 && param <= 20)
+            System.out.println("hello");
+
+        if (param > 5 && param < 15)
+            System.out.println("sup!");
+        System.out.println("bye");
+    }
+
+}
