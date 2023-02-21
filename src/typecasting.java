8d6def3d4f61723d74d936e6c27fb93dd667d7f0
Rakshith Hegde <rakshithhegdeks@gmail.com>
2023-02-21 16:39:13 +0530

    typecasting

---
+++ src/typecasting.java
@@ -0,0 +1,15 @@
+public class typecasting {
+    public static void main(String[] args) {
+        byte b = 127;
+        int a = b;
+        System.out.println(a);
+
+        int c = 894;
+        byte p = (byte) c;
+        System.out.println(p);
+
+        float f = 89.98f;
+        int o = (int) f;
+        System.out.println(o);
+    }
+}
