c0efcd96f73e3cbe06d79c6163af0801f05bbc71
Rakshith Hegde <rakshithhegdeks@gmail.com>
2023-02-21 05:39:28 +0530

    first code

---
+++ src/primitivedatatype.java
@@ -0,0 +1,23 @@
+import java.lang.reflect.Type;
+import java.sql.Types;
+
+public class primitivedatatype {
+    public static void main(String[] args) {
+        byte r = 1;
+        short s = 25;
+        int d = 8974;
+        long t = 56783;
+        float f = 2.674f;
+        double dd = 4.093;
+        char e = 'k';
+        boolean c = false;
+        System.out.println("Byte is a data type in java " + r);
+        System.out.println("short is a data type in java " + s);
+        System.out.println("int is a data type in java " + d);
+        System.out.println("long is a data type in java " + t);
+        System.out.println("float is a data type in java " + f);
+        System.out.println("double is a data type in java " + dd);
+        System.out.println("char is a data type in java " + e);
+        System.out.println("Boolean is a data type in java " + c);
+    }
+}
