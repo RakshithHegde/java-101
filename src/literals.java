82219ba54f2c5fb5b8e23d75400f85a9f443fda7
Rakshith Hegde <rakshithhegdeks@gmail.com>
2023-02-21 16:16:14 +0530

    literals

---
+++ src/literals.java
@@ -0,0 +1,24 @@
+
+public class literals {
+
+    public static void main(String[] args) {
+        int em = 0b01;
+        System.out.println(em);
+
+        int ws = 0x734;
+        System.out.println(ws);
+
+        int uy = 100_000;
+        System.out.println(uy);
+
+        double num1 = 34;
+        System.out.println(num1);
+
+        double nu = 12e31;
+        System.out.println(nu);
+
+        char x = 'a';
+        x++;
+        System.out.println(x);
+    }
+}
